b6ecc8148ed69cbeb0e1b44b24e240b7b8a53769
Alex K <aklimovskikh@yandex.ru>
2025-10-28 15:49:19 +0300

    Test dockerfile

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="app">
-    nginx + vue
-  </div>
+  <div class="app">nginx + vue + 1</div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style>
-</style>
+<style></style>
